84a64f3d62e103ebc87f8448b72349d0539a3cef
Hoangcong1502 <nguyenhoangconghvtc@gmail.com>
2022-09-07 09:50:06 +0700

    update

--- springboot/spring-clazz/src/test/java/edu/hanoi/ApplicationTests.java
+++ springboot/spring-clazz/src/test/java/edu/hanoi/ApplicationTests.java
@@ -1,13 +1,13 @@
-package edu.hanoi.springclazz;
+package edu.hanoi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringClazzApplicationTests {
+class ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
